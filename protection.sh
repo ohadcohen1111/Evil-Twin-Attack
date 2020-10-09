e668836356ec932fe0380ada74d9ae744da36219
ohadcohen1111 <57725202+ohadcohen1111@users.noreply.github.com>
2020-10-09 10:11:15 -0400

    Create protection.sh

---
+++ protection.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+NC='\033[0m' # No Color
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+
+# Monitor Mode
+iwconfig
+echo -e "${BIRed}Please write the name of the Interface:${NC}"
+read interface
+
+printf "\e${Cyan}\nSet Monitor Mode \n \n"
+printf "\e${NC}"
+./monitor-mode.sh $interface
+
+# Start protection script
+python protection.py $interface
